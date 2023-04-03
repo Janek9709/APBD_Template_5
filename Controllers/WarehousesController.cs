515539f4a563401336449b50cdb6d3beb9675d9c
Janek9709 <jan.biniek97@gmail.com>
2023-04-03 22:37:00 +0200

    initial

---
+++ Controllers/WarehousesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Zadanie5.DTOs;
+using Zadanie5.Services;
+
+namespace Zadanie5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehousesController : ControllerBase
+    {
+        private readonly IWarehouseService _warehouseService;
+
+        public WarehousesController(IWarehouseService warehouseService)
+        {
+            _warehouseService = warehouseService;
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(ProductDTO product)
+        {
+            _warehouseService.AddProduct();
+            return Ok();
+        }
+    }
+}
---
+++ DTOs/ProductDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zadanie5.DTOs
+{
+    public class ProductDTO
+    {
+        [Range(0, 10, ErrorMessage = "Amount must be in range 0 - 10")]
+        public int Amount { get; set; }
+    }
+}
